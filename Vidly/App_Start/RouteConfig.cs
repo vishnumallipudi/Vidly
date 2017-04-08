384f3619dd4ee0fefe57cbdfe103cb95a8c66015
vishnumallipudi <vishnumallipudi.584@gmail.com>
2017-04-08 01:31:14 -0400

    Exercise 1

---
+++ Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
+            
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            //routes.MapRoute(
+            //    name:"MoviesList",
+            //    url: "Movies/Movieslist",
+            //    defaults:new { Controller="Movies",Action= "Movieslist",id=UrlParameter.Optional });
+        }
+    }
+}
